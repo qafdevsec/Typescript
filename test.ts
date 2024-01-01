1e707e093f196b52b377b6c6df5f758a4e1b6eb2
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2024-01-01 15:10:48 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,27 +1,10 @@
-import * as tls from 'tls';
-
-function insecureMinMaxTlsVersion() {
-  {
-    const config: tls.ConnectionOptions = {};
-    config.minVersion = 'TLSv1'; // BAD: Choosing the lowest supported version (i.e., SSL3.0 or TLSv1.0)
-  }
-  {
-    const config: tls.ConnectionOptions = {};
-    config.minVersion = 'SSLv3'; // BAD: SSL 3.0 is a non-secure version of the protocol; it's not safe to use it as MinVersion.
-  }
-  {
-    const config: tls.ConnectionOptions = {};
-    config.maxVersion = 'SSLv3'; // BAD: SSL 3.0 is a non-secure version of the protocol; it's not safe to use it as MaxVersion.
-  }
-}
-
-function insecureCipherSuites() {
-  const config: tls.ConnectionOptions = {
-    ciphers: ['TLS_RSA_WITH_RC4_128_SHA'], // BAD: Non-secure cipher suite TLS_RSA_WITH_RC4_128_SHA
-  };
-  // Do something with the config
+function insecurePassword(): string {
+  // BAD: the random suffix is not cryptographically secure
+  const suffix: number = Math.random();
+  const password: string = "myPassword" + suffix;
+  return password;
 }
 
 // Example usage
-insecureMinMaxTlsVersion();
-insecureCipherSuites();
+const generatedPassword: string = insecurePassword();
+console.log('Generated Password:', generatedPassword);
