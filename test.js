937b110df7ca9f241cc7afbbf2e28ac611d43f2d
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2024-01-01 15:26:35 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,11 @@
-function generateReceiptURL(baseUrl: string): string {
-  const ranGen = new RandomGenerator();
-  ranGen.setSeed(new Date().getTime());
-  return baseUrl + ranGen.nextInt(400000000) + ".html";
+async function updateUser(userId, requestBody) {
+  const userData = await db.loadUserData(userId);
+  merge(userData, requestBody);
+
+  log("Saving userData " + userData.toString());
+  await db.saveUserData(userId, userData);
+  return userData;
 }
 
-class RandomGenerator {
-  private seed: number;
-
-  constructor() {
-    this.seed = 0;
-  }
-
-  setSeed(seed: number): void {
-    this.seed = seed;
-  }
-
-  nextInt(max: number): number {
-    this.seed = (this.seed * 9301 + 49297) % 233280;
-    return Math.floor(this.seed / 233280 * max);
-  }
-}
-
-// Example usage
-const baseUrl = 'https://example.com/';
-const receiptURL = generateReceiptURL(baseUrl);
-console.log('Generated Receipt URL:', receiptURL);
+async function getRole(userId) {
+  const userPermissions = await db.loadUserPermissions(userId);
